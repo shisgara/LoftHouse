3c41aa80d304d51e1954043efe454515d9c95eac
shisgara <118660637+shisgara@users.noreply.github.com>
2023-05-29 14:38:52 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,82 @@
+/* Yandex Map */
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+
+ymaps.ready(init);
+function init() {
+	// Создание карты.
+	let myMap = new ymaps.Map('first_map', {
+		// Координаты центра карты.
+		// Порядок по умолчанию: «широта, долгота».
+		// Чтобы не определять координаты центра карты вручную,
+		// воспользуйтесь инструментом Определение координат.
+		center: [59.943543, 30.338928],
+		// Уровень масштабирования. Допустимые значения:
+		// от 0 (весь мир) до 19.
+		zoom: 16,
+	});
+
+	let myPlacemark = new ymaps.Placemark([59.943543, 30.338928],
+		{
+			balloonContent:
+				`
+				<div class="balloon">
+					<div class="balloon__address">Наб. реки Фонтанки, 10-15</div>
+					<div class="balloon__contacts">
+						<a href="tel:+78121234567">+7 (812) 123-45-67</a>
+					</div>
+				</div>
+			`
+		},
+		{
+			iconLayout: 'default#image',
+			iconImageHref: './img/map/location-pin.svg',
+			iconImageSize: [40, 40],
+			iconImageOffset: [-20, -40]
+		});
+
+		myMap.controls.remove('geolocationControl'); // удаляем геолокацию
+		myMap.controls.remove('searchControl'); // удаляем поиск
+		myMap.controls.remove('trafficControl'); // удаляем контроль трафика
+		myMap.controls.remove('typeSelector'); // удаляем тип
+	
+		// myMap.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+		// myMap.controls.remove('zoomControl'); // удаляем контрол зуммирования
+		myMap.controls.remove('rulerControl'); // удаляем контрол правил
+		myMap.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+	myMap.geoObjects.add(myPlacemark);
+	myPlacemark.balloon.open();
+}
+
+
+
+
+
+
+const navBtn = document.querySelector('.nav-icon-btn');
+const navIcon = document.querySelector('.nav-icon');
+const nav = document.querySelector('.header__top-row');
+
+navBtn.onclick = function () {
+	navIcon.classList.toggle('nav-icon--active');
+	nav.classList.toggle('header__top-row--mobile');
+	document.body.classList.toggle('no-scroll');
+}
+
+// PhoneMask
+
+mask('[data-tel-input]');
+
+// Удаляем '+' если больше ничего не введено, чтобы показать placeholder
+const phoneInputs = document.querySelectorAll('[data-tel-input]');
+phoneInputs.forEach((input) => {
+	input.addEventListener('input', () => {
+		if (input.value == '+') input.value = '';
+	})
+	input.addEventListener('blur', () => {
+		if (input.value == '+') input.value = '';
+	})
+});
+
